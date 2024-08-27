5360d7e5d117534171dc6b1ededec23c02e68c57
green <green@mobidoc.us>
2024-08-27 17:19:29 +0900

    feat : refactoring SRP

--- src/main/java/cleancode/minesweeper/AppException.java
+++
@@ -1,9 +0,0 @@
-package cleancode.minesweeper;
-
-public class AppException extends RuntimeException {
-
-    public AppException(String message) {
-        super(message);
-
-    }
-}
---
+++ src/main/java/cleancode/minesweeper/GameException.java
@@ -0,0 +1,9 @@
+package cleancode.minesweeper;
+
+public class GameException extends RuntimeException {
+
+    public GameException(String message) {
+        super(message);
+
+    }
+}
