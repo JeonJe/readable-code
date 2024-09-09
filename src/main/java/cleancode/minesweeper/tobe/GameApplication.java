9af141d91674ac9cd5390a78971ec145d2b525f5
green <green@mobidoc.us>
2024-09-09 22:29:25 +0900

    feat : 숨겨져 있는 개념 도출하기(game 설정 정보)

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -2,6 +2,7 @@ package cleancode.minesweeper.tobe;
 
 import cleancode.minesweeper.asis.gamelevel.Beginner;
 import cleancode.minesweeper.asis.gamelevel.GameLevel;
+import cleancode.minesweeper.tobe.config.GameConfig;
 import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
 import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
 import cleancode.minesweeper.tobe.io.InputHandler;
@@ -9,12 +10,15 @@ import cleancode.minesweeper.tobe.io.OutputHandler;
 
 public class GameApplication {
 
-    public static void main(String[] args) {
-        GameLevel gameLevel = new Beginner();
-        InputHandler inputHandler = new ConsoleInputHandler();
-        OutputHandler outputHandler = new ConsoleOutputHandler();
 
-        Minesweeper mineSweeper = new Minesweeper(gameLevel, inputHandler, outputHandler);
+
+    public static void main(String[] args) {
+        GameConfig gameConfig = new GameConfig(
+                new Beginner(),
+                new ConsoleInputHandler(),
+                new ConsoleOutputHandler()
+        );
+        Minesweeper mineSweeper = new Minesweeper(gameConfig);
         mineSweeper.initialize();
         mineSweeper.run();
     }
